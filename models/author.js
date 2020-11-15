103e349d7ccad5c324cb64f29c628b41a1d24b20
Matt Haffner <mrhaffner13@gmail.com>
2020-11-14 21:12:57 -0600

    updated the controller and view template for author_list. added luxon dates formatting

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -30,6 +31,18 @@ AuthorSchema
 .virtual('url')
 .get(function () {
   return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 //Export model
