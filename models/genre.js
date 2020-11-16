208800c4f5bee59554a98128b8575c9b2284cfb6
Matt Haffner <mrhaffner13@gmail.com>
2020-11-15 21:53:19 -0600

    updated author mode and controller to allow for updating authors

--- models/genre.js
+++ models/genre.js
@@ -1,22 +1,3 @@
-// var mongoose = require('mongoose');
-
-// var Schema = mongoose.Schema;
-
-// var GenreSchema = new Schema(
-//     {
-//         name: {type: String, required: true, minlength: 3, maxlength: 100}
-//     }
-// );
-
-// GenreSchema
-//     .virtual('url')
-//     .get(() => `/catalog/genre/${this._id}`)
-
-// module.exports = mongoose.model('Genre', GenreSchema);
-
-
-
-
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
